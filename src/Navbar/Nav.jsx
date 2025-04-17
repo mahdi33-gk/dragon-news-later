86b7dad089998c35f28c18f64d948cab72049819
mehedi <mehedi25122003@gmail.com>
2025-04-17 21:44:48 +0600

    header and main logo added

--- src/Navbar/Nav.jsx
+++ src/Navbar/Nav.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import logo from "../images/logo.png";
 
 const Nav = () => {
-    return (
-        <div>
-            <h1>this is nav</h1>
+  return (
+    <div>
+      <header className="flex flex-col">
+        <div className="flex justify-center items-center">
+          <img className="w-1/4" src={logo} alt="logo" />
         </div>
-    );
+      </header>
+      <main></main>
+    </div>
+  );
 };
 
 export default Nav;
