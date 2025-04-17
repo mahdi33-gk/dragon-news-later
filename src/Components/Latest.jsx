0da71a403d15259cd739a8966bebc56e2405eb15
mehedi <mehedi25122003@gmail.com>
2025-04-17 22:29:37 +0600

    marquee installed and use

--- src/Components/Latest.jsx
+++ src/Components/Latest.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Marquee from 'react-fast-marquee';
 
 const Latest = () => {
     return (
         <div className='flex gap-2 px-3 py-1 mt-5'>
             <button className='btn btn-sm bg-rose-600 text-white'>Latest</button>
-            <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quas sequi dolore reprehenderit, id assumenda velit nesciunt mollitia quidem, quisquam autem quo perferendis deleniti tenetur molestias saepe animi itaque!</p>
+    
+            <Marquee>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quas sequi dolore reprehenderit, id assumenda velit nesciunt mollitia quidem, quisquam autem quo perferendis deleniti tenetur molestias saepe animi itaque!
+            </Marquee>
         </div>
     );
 };
