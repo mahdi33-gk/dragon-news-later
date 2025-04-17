08e261615250ceaa0a1be238927efd05c24e47b9
mehedi <mehedi25122003@gmail.com>
2025-04-17 22:04:17 +0600

    moment.js use for the first time

--- src/Navbar/Nav.jsx
+++ src/Navbar/Nav.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import logo from "../images/logo.png";
+import moment from "moment";
 
 const Nav = () => {
   return (
     <div>
-      <header className="flex flex-col">
+      <header className="flex flex-col gap-2">
         <div className="flex justify-center items-center">
-          <img className="w-1/4" src={logo} alt="logo" />
+          <img className="w-2/5" src={logo} alt="logo" />
         </div>
+        <p className="text-gray-500">Journalism Without Fear or Favour</p>
+        <p className="text-gray-500 font-bold">{moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
       </header>
+      <nav></nav>
       <main></main>
     </div>
   );
