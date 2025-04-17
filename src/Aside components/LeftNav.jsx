c9f0b352251938aac3e58050c51c7bc077d9be3d
mehedi <mehedi25122003@gmail.com>
2025-04-17 23:43:13 +0600

    category section added

--- src/Aside components/LeftNav.jsx
+++ src/Aside components/LeftNav.jsx
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const LeftNav = () => {
-    const [categories,setCategories] = useState([]);
-    useEffect(()=>{
-        fetch('https://openapi.programming-hero.com/api/news/categories')
-        .then(res=>res.json())
-        .then(data=>setCategories(data.data.news_category))
-    },[])
-    return (
-        <div>
-            <h1 className='text-[20px] font-semibold'>All Caterogy ({categories.length})</h1>
-{/* category btn */}
-            <div>
-                {
-                    categories.map((category)=> (<button key={category.category_id}>{category.category_name}</button>))
-                }
-            </div>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("https://openapi.programming-hero.com/api/news/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data.data.news_category));
+  }, []);
+  return (
+    <div>
+      <h1 className="text-[20px] font-semibold">
+        All Caterogy ({categories.length})
+      </h1>
+      {/* category btn */}
+      <div className="flex flex-col gap-2 mt-3">
+        {categories.map((category) => (
+          <button className="btn" key={category.category_id}>{category.category_name}</button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
