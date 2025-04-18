84ed00e0e72dd86aa143bd323f50ba9e32717a1b
mehedi <mehedi25122003@gmail.com>
2025-04-18 15:00:16 +0600

    social media linki done

--- src/Components/SocaialLink.jsx
+++ src/Components/SocaialLink.jsx
@@ -3,10 +3,10 @@ import { CiFacebook, CiInstagram, CiTwitter } from "react-icons/ci";
 
 const SocaialLink = () => {
     return (
-        <div>
-            <button className='btn w-full'><CiFacebook></CiFacebook> Facebook</button>
-            <button className='btn w-full'><CiInstagram></CiInstagram> Instagram</button>
-            <button className='btn w-full'><CiTwitter></CiTwitter> Twitter</button>
+        <div className='*:bg-base-100'>
+            <button className='btn w-full justify-start'><CiFacebook></CiFacebook> Facebook</button>
+            <button className='btn w-full justify-start'><CiInstagram></CiInstagram> Instagram</button>
+            <button className='btn w-full justify-start'><CiTwitter></CiTwitter> Twitter</button>
         </div>
     );
 };
