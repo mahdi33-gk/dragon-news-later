5bd0694ea26e3318e18d821fec2c38cf2696a77d
mehedi <mehedi25122003@gmail.com>
2025-04-19 23:36:33 +0600

    env added but not working

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBPFbkSTZQMHLlb8HkMl70-pt31mOBQksE",
-  authDomain: "dragon-news-c4d23.firebaseapp.com",
-  projectId: "dragon-news-c4d23",
-  storageBucket: "dragon-news-c4d23.firebasestorage.app",
-  messagingSenderId: "708547882429",
-  appId: "1:708547882429:web:6feeaccbaf15e1e581f6d4",
-  measurementId: "G-2ZG1NDHC3C"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
